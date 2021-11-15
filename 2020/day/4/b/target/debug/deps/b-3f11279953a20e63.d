4f0943e00b0ad330dbd4a7c1546a501ea3689365
t56k <txnash@gmail.com>
2021-11-16 10:01:18 +1300

    add gitignore

--- 2020/day/4/b/target/debug/deps/b-3f11279953a20e63.d
+++
@@ -1,6 +0,0 @@
-/Users/thomasnash/build/advent-of-code/2020/day/4/b/target/debug/deps/b-3f11279953a20e63.rmeta: src/main.rs src/../input
-
-/Users/thomasnash/build/advent-of-code/2020/day/4/b/target/debug/deps/b-3f11279953a20e63.d: src/main.rs src/../input
-
-src/main.rs:
-src/../input:
--- 2020/day/4/b/target/debug/deps/b-87db8e690e42a422.d
+++
@@ -1,6 +0,0 @@
-/Users/thomasnash/build/advent-of-code/2020/day/4/b/target/debug/deps/b-87db8e690e42a422.rmeta: src/main.rs src/../input
-
-/Users/thomasnash/build/advent-of-code/2020/day/4/b/target/debug/deps/b-87db8e690e42a422.d: src/main.rs src/../input
-
-src/main.rs:
-src/../input:
