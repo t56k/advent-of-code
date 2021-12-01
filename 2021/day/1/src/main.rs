4a07707ec2bd2fa87e10a2142d51e1a62370a447
t56k <txnash@gmail.com>
2021-12-02 10:39:13 +1300

    finish 1.1

--- 2021/day/1/src/main.rs
+++ 2021/day/1/src/main.rs
@@ -1,38 +1,25 @@
-use std::fs;
+ use std::fs;
 
 pub fn main() {
     let input = fs::read_to_string("./input").expect("Couldn't read input");
     let contents = parse_input(input);
-    // let groups = contents.chunks(25);
+    let n = contents.len();
+    let mut counter = 0;
 
-    let res = check_for_factors(&contents, 222);
-    println!("{}", res);
+    for i in 0..n {
+        match contents.get(i + 1) {
+            Some(x) => {
+                if x > &contents[i] {
+                    counter += 1;
+                }
+            },
+            None => ()
+        }
+    }
+
+    println!("part a: {:?}", counter);
 }
-
-// fn check_for_factors(contents: Vec<isize>, target: isize) -> bool {
-//     contents.iter().enumerate().any(|(i, &x)| {
-//         contents
-//             .iter()
-//             .enumerate()
-//             .filter(|&(j, _)| i != j)
-//             .any(|(_, &y)| x + y == target)
-//     })
-// }
 
 fn parse_input(input: String) -> Vec<isize> {
     input.lines().map(|l| l.parse().unwrap()).collect()
-}
-
-fn check_for_factors(contents: Vec<isize>, target: isize) -> bool {
-    let n = contents.len();
-    for i in 0..n {
-        let x = contents[i];
-        for j in (i + 1)..n {
-            let y = contents[j];
-            if x + y == target {
-                return true;
-            }
-        }
-    }
-    false
 }
