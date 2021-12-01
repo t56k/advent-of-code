f644b0e3e6e045d506b469fd9acf6c7d0de671dd
t56k <txnash@gmail.com>
2021-12-02 12:15:28 +1300

    finish part b with .zip

--- 2021/day/1/src/main.rs
+++ 2021/day/1/src/main.rs
@@ -1,17 +1,20 @@
 use std::fs;
 
+// learned about https://doc.rust-lang.org/std/iter/struct.Zip.html
 pub fn main() {
     let input = fs::read_to_string("./input").expect("Couldn't read input");
     let contents = parse_input(input);
 
-    println!("part a: {}",
-             contents
-                .iter()
-                .zip(contents[1..].iter())
-                .filter(|( a, b )| a < b)
-                .count()
-    );
+    println!("part a: {}", count_windows(&contents, 1));
+    println!("part b: {}", count_windows(&contents, 3));
+}
 
+fn count_windows(contents: &Vec<usize>, span: usize) -> usize {
+    contents
+        .iter()
+        .zip(contents[span..].iter())
+        .filter(|( a, b )| a < b)
+        .count()
 }
 
 fn parse_input(input: String) -> Vec<usize> {
