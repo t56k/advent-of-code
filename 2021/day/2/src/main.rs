057314fe075888c4812a3e8685c9dbdb690eed19
t56k <txnash@gmail.com>
2021-12-02 18:31:38 +1300

    neater part a day 2

--- 2021/day/2/src/main.rs
+++ 2021/day/2/src/main.rs
@@ -4,28 +4,18 @@ pub fn main() {
     let input = fs::read_to_string("./input").expect("Couldn't read input");
     let contents = input.lines().collect::<Vec<_>>();
 
-    let horizontal =
-        contents
-            .iter()
-            .filter(|el| el.contains("forward"))
-            .fold(0, |acc, el| acc
-                + el.split(" ").nth(1).unwrap().parse::<i32>().unwrap());
-
-    let up =
-        contents
-            .iter()
-            .filter(|el| el.contains("up"))
-            .fold(0, |acc, el| acc
-                + el.split(" ").nth(1).unwrap().parse::<i32>().unwrap());
-
-    let down =
-        contents
-            .iter()
-            .filter(|el| el.contains("down"))
-            .fold(0, |acc, el| acc
-                + el.split(" ").nth(1).unwrap().parse::<i32>().unwrap());
-
+    let horizontal = calculate_by_type(&contents, "forward");
+    let up = calculate_by_type(&contents, "up");
+    let down = calculate_by_type(&contents, "down");
     let depth = down - up;
 
     println!("{}", horizontal * depth);
+}
+
+fn calculate_by_type(contents: &Vec<&str>, direction: &str) -> i32 {
+    contents
+        .iter()
+        .filter(|el| el.contains(direction))
+        .fold(0, |acc, el| acc
+            + el.split(" ").nth(1).unwrap().parse::<i32>().unwrap())
 }
