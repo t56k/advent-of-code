a6195e90533306897150a6a28e94ad568821a7b7
t56k <txnash@gmail.com>
2021-12-02 19:27:01 +1300

    day 2 part 2 done messily

--- 2021/day/2/src/main.rs
+++ 2021/day/2/src/main.rs
@@ -9,13 +9,43 @@ pub fn main() {
     let down = calculate_by_type(&contents, "down");
     let depth = down - up;
 
-    println!("{}", horizontal * depth);
+    println!("part a: {}", horizontal * depth);
+    println!("part b: {}", calculate_aim_and_depth(&contents));
+
 }
 
+// part a
 fn calculate_by_type(contents: &Vec<&str>, direction: &str) -> i32 {
     contents
         .iter()
         .filter(|el| el.contains(direction))
         .fold(0, |acc, el| acc
             + el.split(" ").nth(1).unwrap().parse::<i32>().unwrap())
+}
+
+// part b
+fn calculate_aim_and_depth(contents: &Vec<&str>) -> i32 {
+    let mut aim = 0;
+    let mut depth = 0;
+    let mut horizontal = 0;
+
+    contents
+        .iter()
+        .for_each(|el| {
+            let parts = el.split(" ").collect::<Vec<_>>();
+            let dir = parts[0];
+            let val = parts[1].parse::<i32>().unwrap();
+
+            match dir {
+                "down" => aim += val,
+                "up" => aim -= val,
+                "forward" => {
+                    horizontal += val;
+                    depth += aim * val;
+                },
+                _ => ()
+            }
+        });
+
+    horizontal * depth
 }
