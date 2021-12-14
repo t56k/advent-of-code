6f4762863df11f684909d115d10998d7afa87b1e
t56k <txnash@gmail.com>
2021-12-15 12:22:53 +1300

    fix tests

--- 2021/14/src/main.rs
+++ 2021/14/src/main.rs
@@ -3,29 +3,7 @@ use std::mem;
 pub fn main() {
     let (polymer, insertions) = include_str!("../input").split_once("\n\n").unwrap();
     let polymer = polymer.as_bytes().to_vec();
-
-    let mut insertions = insertions
-        .lines()
-        .map(|l| {
-            let (k, t) = l.split_once(" -> ").unwrap();
-            let (k, t) = (k.as_bytes(), t.as_bytes()[0]);
-            ([k[0], k[1]], [k[0], t])
-        })
-        .collect::<Vec<_>>();
-    insertions.sort_unstable_by_key(|r| r.0);
-
-    let insertion = insertions
-        .iter()
-        .map(|r| {
-            (
-                r.0,
-                insertions.binary_search_by_key(&r.1, |r| r.0).unwrap(),
-                insertions
-                    .binary_search_by_key(&[r.1[1], r.0[1]], |r| r.0)
-                    .unwrap(),
-            )
-        })
-        .collect::<Vec<_>>();
+    let insertion = parse_insertions(insertions);
 
     let part_one = process(polymer.clone(), insertion.clone(), 10);
     let part_two = process(polymer, insertion, 40);
@@ -63,4 +41,64 @@ fn process(polymer: Vec<u8>, insertion: Vec<([u8; 2], usize, usize)>, count: u64
         .fold((u64::MAX, 0), |(min, max), &n| (min.min(n), max.max(n)));
 
     max - min
+}
+
+fn parse_insertions(insertions: &str) -> Vec<([u8; 2], usize, usize)> {
+    let mut insertions = insertions
+        .lines()
+        .map(|l| {
+            let (k, t) = l.split_once(" -> ").unwrap();
+            let (k, t) = (k.as_bytes(), t.as_bytes()[0]);
+            ([k[0], k[1]], [k[0], t])
+        })
+        .collect::<Vec<_>>();
+    insertions.sort_unstable_by_key(|r| r.0);
+
+    insertions
+        .iter()
+        .map(|r| {
+            (
+                r.0,
+                insertions.binary_search_by_key(&r.1, |r| r.0).unwrap(),
+                insertions
+                    .binary_search_by_key(&[r.1[1], r.0[1]], |r| r.0)
+                    .unwrap(),
+            )
+        })
+        .collect::<Vec<_>>()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn part_process_example() {
+        let (polymer, insertions) = "NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C"
+            .split_once("\n\n")
+            .unwrap();
+
+        let polymer = polymer.as_bytes().to_vec();
+        let insertion = parse_insertions(insertions);
+
+        assert_eq!(process(polymer.clone(), insertion.clone(), 10), 1588);
+        assert_eq!(process(polymer, insertion, 40), 2188189693529);
+    }
 }
