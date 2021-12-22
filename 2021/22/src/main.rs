45c038dab79aa86e8410a26f4198f997f565c10c
t56k <txnash@gmail.com>
2021-12-23 09:55:24 +1300

    cuboids!

--- 2021/22/src/main.rs
+++ 2021/22/src/main.rs
@@ -1,77 +1,173 @@
-use std::cmp::{max, min};
-use std::collections::*;
 use std::time::Instant;
 
-type Range = std::ops::RangeInclusive<isize>;
-type CubeRange = Vec<(bool, Range, Range, Range)>;
-
-fn parse(data: &str) -> CubeRange {
-    data.lines()
-        .map(|line| {
-            let (state, ranges) = line.split_once(' ').unwrap();
-            let state = if state == "on" {
-                true
-            } else if state == "off" {
-                false
-            } else {
-                unreachable!();
-            };
-
-            let mut xyz = ranges.split(',').map(|rng| {
-                let rng = rng.split_once('=').unwrap().1;
-                let mut rng = rng.split("..").map(|x| x.parse().unwrap());
-                let a = rng.next().unwrap();
-                let b = rng.next().unwrap();
-                a..=b
-            });
-
-            (
-                state,
-                xyz.next().unwrap(),
-                xyz.next().unwrap(),
-                xyz.next().unwrap(),
-            )
-        })
-        .collect::<Vec<_>>()
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct Cuboid {
+    x_min: i32,
+    x_max: i32,
+    y_min: i32,
+    y_max: i32,
+    z_min: i32,
+    z_max: i32,
 }
 
-fn part_one(cube_ranges: &CubeRange) -> isize {
-    let mut on_cubes = HashSet::new();
-    for cube in cube_ranges.iter() {
-        on_cubes = calc_part_one(&on_cubes, cube);
+impl From<&str> for Cuboid {
+    fn from(s: &str) -> Self {
+        let c = s
+            .split(',')
+            .flat_map(|p| {
+                p.split('=')
+                    .nth(1)
+                    .unwrap()
+                    .split("..")
+                    .map(|v| v.parse::<i32>().unwrap())
+            })
+            .collect::<Vec<_>>();
+        Cuboid::new(c[0], c[1], c[2], c[3], c[4], c[5])
     }
-
-    on_cubes.len() as isize
 }
 
-fn calc_part_one(
-    on_cubes: &HashSet<(isize, isize, isize)>,
-    instr: &(bool, Range, Range, Range),
-) -> HashSet<(isize, isize, isize)> {
-    let mut res = on_cubes.clone();
-    let (state, xr, yr, zr) = instr;
-
-    for x in max(-50, *xr.start())..=min(50, *xr.end()) {
-        for y in max(-50, *yr.start())..=min(50, *yr.end()) {
-            for z in max(-50, *zr.start())..=min(50, *zr.end()) {
-                if *state {
-                    res.insert((x, y, z));
-                } else {
-                    res.remove(&(x, y, z));
-                }
-            }
+impl Cuboid {
+    pub fn new(x_min: i32, x_max: i32, y_min: i32, y_max: i32, z_min: i32, z_max: i32) -> Self {
+        Cuboid {
+            x_min,
+            x_max,
+            y_min,
+            y_max,
+            z_min,
+            z_max,
         }
     }
 
-    res
+    pub fn intersect_split(&mut self, other: &Cuboid) -> Vec<Cuboid> {
+        let mut result_vec = Vec::new();
+        if (self.x_min <= other.x_max && self.x_max >= other.x_min)
+            && (self.y_min <= other.y_max && self.y_max >= other.y_min)
+            && (self.z_min <= other.z_max && self.z_max >= other.z_min)
+        {
+            // on x
+            if self.x_min < other.x_min {
+                result_vec.push(Cuboid::new(
+                    self.x_min,
+                    other.x_min - 1,
+                    self.y_min,
+                    self.y_max,
+                    self.z_min,
+                    self.z_max,
+                ));
+                self.x_min = other.x_min;
+            }
+            if self.x_max > other.x_max {
+                result_vec.push(Cuboid::new(
+                    other.x_max + 1,
+                    self.x_max,
+                    self.y_min,
+                    self.y_max,
+                    self.z_min,
+                    self.z_max,
+                ));
+                self.x_max = other.x_max;
+            }
+            // on y
+            if self.y_min < other.y_min {
+                result_vec.push(Cuboid::new(
+                    self.x_min,
+                    self.x_max,
+                    self.y_min,
+                    other.y_min - 1,
+                    self.z_min,
+                    self.z_max,
+                ));
+                self.y_min = other.y_min;
+            }
+            if self.y_max > other.y_max {
+                result_vec.push(Cuboid::new(
+                    self.x_min,
+                    self.x_max,
+                    other.y_max + 1,
+                    self.y_max,
+                    self.z_min,
+                    self.z_max,
+                ));
+                self.y_max = other.y_max;
+            }
+            // on z
+            if self.z_min < other.z_min {
+                result_vec.push(Cuboid::new(
+                    self.x_min,
+                    self.x_max,
+                    self.y_min,
+                    self.y_max,
+                    self.z_min,
+                    other.z_min - 1,
+                ));
+                self.z_min = other.z_min;
+            }
+            if self.z_max > other.z_max {
+                result_vec.push(Cuboid::new(
+                    self.x_min,
+                    self.x_max,
+                    self.y_min,
+                    self.y_max,
+                    other.z_max + 1,
+                    self.z_max,
+                ));
+                self.z_max = other.z_max;
+            }
+        } else {
+            result_vec.push(*self)
+        }
+        result_vec
+    }
+
+    pub fn area(&self) -> usize {
+        let mut result = 1;
+        result *= (self.x_max - self.x_min) as usize + 1;
+        result *= (self.y_max - self.y_min) as usize + 1;
+        result *= (self.z_max - self.z_min) as usize + 1;
+        result
+    }
+}
+
+fn parse(input: &str) -> Vec<Cuboid> {
+    input.lines().fold(Vec::<Cuboid>::new(), |mut acc, line| {
+        let (switch_str, coords) = line.split_once(' ').unwrap();
+        let switch = switch_str == "on";
+        let mut cuboids = Vec::with_capacity(acc.len() + 24);
+        let parsed_cuboid = Cuboid::from(coords);
+        for oc in acc.iter_mut() {
+            cuboids.append(&mut oc.intersect_split(&parsed_cuboid));
+        }
+        if switch {
+            cuboids.push(parsed_cuboid);
+        }
+        cuboids
+    })
+}
+
+fn part_one(v: &[Cuboid]) -> usize {
+    v.iter()
+        .filter(|c| {
+            c.x_min >= -50
+                && c.x_max <= 50
+                && c.y_min >= -50
+                && c.y_max <= 50
+                && c.z_min >= -50
+                && c.z_max <= 50
+        })
+        .map(|c| c.area())
+        .sum()
+}
+
+fn part_two(v: &[Cuboid]) -> usize {
+    v.iter().map(|c| c.area()).sum()
 }
 
 fn main() {
     let input = include_str!("../input");
-    let cube_ranges = parse(input);
-    let part_one = part_one(&cube_ranges);
+    let cubes = parse(&input);
 
-    benchmark(|| println!("part one: {}", part_one));
+    benchmark(|| println!("part one: {}", part_one(&cubes)));
+    benchmark(|| println!("part two: {}", part_two(&cubes)));
 }
 
 fn benchmark<F, T>(f: F) -> T
@@ -87,8 +183,16 @@ where
 
 #[test]
 fn check_part_one() {
-    let input = include_str!("../example");
-    let cube_ranges = parse(input);
+    let input = include_str!("../example_one");
+    let cubes = parse(&input);
 
-    assert_eq!(590784, part_one(&cube_ranges));
+    assert_eq!(590784, part_one(&cubes));
+}
+
+#[test]
+fn check_part_two() {
+    let input = include_str!("../example_two");
+    let cubes = parse(&input);
+
+    assert_eq!(2758514936282235, part_two(&cubes));
 }
