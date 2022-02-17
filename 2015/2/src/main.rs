8345ee0f8a609ac2e44c87b4a3ef06dd8f581913
t56k <txnash@gmail.com>
2022-02-18 09:11:00 +1300

    2015 day 2 part 2, refactor

--- 2015/2/src/main.rs
+++ 2015/2/src/main.rs
@@ -5,24 +5,23 @@ fn main() {
     let input = include_str!("../input");
 
     println!("part one: {}", part_one(&input));
+    println!("part two: {}", part_two(&input));
 
     let end = start.elapsed();
     println!("time: {:}us", end.as_micros());
 }
 
-pub fn part_one(input: &str) -> u32 {
+fn part_one(input: &str) -> u32 {
     input.lines().into_iter().fold(0, |acc, line| {
-        let sides = line
-            .split('x')
-            .map(|x| x.parse::<u32>().unwrap())
-            .collect::<Vec<u32>>();
-
+        let sides = parse_line(line);
         let area = sides
             .iter()
             .enumerate()
             .map(|(i, _)| {
                 let mut j = i + 1;
-                if j == 3 { j = 0 }
+                if j == sides.len() {
+                    j = 0
+                }
 
                 sides[i] * sides[j]
             })
@@ -33,8 +32,32 @@ pub fn part_one(input: &str) -> u32 {
     })
 }
 
+fn part_two(input: &str) -> u32 {
+    input.lines().into_iter().fold(0, |acc, line| {
+        let sides = parse_line(line);
+        let mut smallest = sides.clone();
+
+        smallest.sort();
+        smallest.pop();
+
+        acc + ((2 * smallest.iter().sum::<u32>()) + sides.iter().product::<u32>())
+    })
+}
+
+fn parse_line(line: &str) -> Vec<u32> {
+    line.split('x')
+        .map(|x| x.parse::<u32>().unwrap())
+        .collect::<Vec<u32>>()
+}
+
 #[test]
 fn check_part_one() {
     let input = include_str!("../example_one");
     assert_eq!(101, part_one(&input));
+}
+
+#[test]
+fn check_part_two() {
+    let input = include_str!("../example_two");
+    assert_eq!(48, part_two(&input));
 }
