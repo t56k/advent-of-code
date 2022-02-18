487539f5772b2024b9c6081099a0f829f7d8bde2
t56k <txnash@gmail.com>
2022-02-18 13:41:46 +1300

    day 3 part 2

--- 2015/3/src/main.rs
+++ 2015/3/src/main.rs
@@ -6,6 +6,7 @@ fn main() {
     let input = include_str!("../input");
 
     println!("part one: {}", part_one(&input));
+    println!("part two: {}", part_two(&input));
 
     let end = start.elapsed();
     println!("time: {:}us", end.as_micros());
@@ -27,6 +28,43 @@ fn part_one(input: &str) -> u32 {
         };
 
         *grid.entry(curr).or_insert(0) += 1;
+    }
+
+    grid.iter().filter(|&(_, &v)| v > 0).count() as u32
+}
+
+fn part_two(input: &str) -> u32 {
+    let moves = input.lines().next().unwrap().chars();
+    let robo_moves = moves.clone().skip(1).step_by(2);
+
+    let mut grid = HashMap::new();
+    let mut santa_curr = (0, 0);
+    let mut robo_santa_curr = (0, 0);
+
+    grid.insert(santa_curr, 1);
+
+    for m in moves.step_by(2) {
+        santa_curr = match m {
+            '<' => (santa_curr.0 - 1, santa_curr.1),
+            '>' => (santa_curr.0 + 1, santa_curr.1),
+            '^' => (santa_curr.0, santa_curr.1 - 1),
+            'v' => (santa_curr.0, santa_curr.1 + 1),
+            _ => unreachable!(),
+        };
+
+        *grid.entry(santa_curr).or_insert(0) += 1;
+    }
+
+    for m in robo_moves {
+        robo_santa_curr = match m {
+            '<' => (robo_santa_curr.0 - 1, robo_santa_curr.1),
+            '>' => (robo_santa_curr.0 + 1, robo_santa_curr.1),
+            '^' => (robo_santa_curr.0, robo_santa_curr.1 - 1),
+            'v' => (robo_santa_curr.0, robo_santa_curr.1 + 1),
+            _ => unreachable!(),
+        };
+
+        *grid.entry(robo_santa_curr).or_insert(0) += 1;
     }
 
     grid.iter().filter(|&(_, &v)| v > 0).count() as u32
